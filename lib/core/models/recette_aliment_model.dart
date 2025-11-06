7abfd335803fa20fe30cdbc428bf8c79aeeddaee
DevKosX <kosbarmohamed.31@gmail.com>
2025-11-06 11:41:25 +0100

    Model Recette Aliment GETTER ajouté

--- lib/core/models/recette_aliment_model.dart
+++ lib/core/models/recette_aliment_model.dart
@@ -2,6 +2,7 @@
 /// Author: Mohamed KOSBAR
 /// Implémentation du 5/6 novembre 2025
 
+/// ce modele est une classe pivot qui sert de liaison entre les recettess et les aliments dans ces recettes
 
 class RecetteAliment {
   int id_RecetteAliment;
@@ -9,10 +10,41 @@ class RecetteAliment {
   String unite;
   String remarque;
 
+  // --- CONSTRUCTEUR ---
+
   RecetteAliment({
     required this.id_RecetteAliment,
     required this.quantite,
     required this.unite,
     required this.remarque,
   });
+
+
+// --- GETTERS ---
+
+
+  int getIdRecetteAliment() {
+    return id_RecetteAliment;
+  }
+
+  double getQuantite() {
+    return quantite;
+  }
+
+  String getUnite() {
+    return unite;
+  }
+
+  String getRemarque() {
+    return remarque;
+  }
+
+  // J'ajoute les getters pour les IDs de liaison on en aura sûrement besoin les gars.
+  int getIdRecette() {
+    return id_recette;
+  }
+  // pareil ici aussi
+  int getIdAliment() {
+    return id_aliment;
+  }
 }
